5637f1b8c9faabd329483426f22c9f8b5992ec3a
AugustoMagalhaes <augustommg@gmail.com>
2022-07-20 17:32:38 -0300

    feat: implementa model user.

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define(
+		'User',
+		{
+			id: { type: DataTypes.INTEGER, primaryKey: true },
+			displayName: DataTypes.STRING,
+			email: DataTypes.STRING,
+			password: DataTypes.STRING,
+			image: DataTypes.STRING,
+		},
+		{
+			timestamps: false,
+			tableName: 'Users',
+		},
+	);
+
+	return User;
+};
