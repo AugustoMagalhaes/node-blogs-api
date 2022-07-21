a1fbb6456a3a6776eeec732602ade7ed094f0703
AugustoMagalhaes <augustommg@gmail.com>
2022-07-21 04:52:11 -0300

    feat: altera validaçao de login.

--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,21 +1,21 @@
-const { User } = require('../database/models/user');
-
 const validateUserFields = (req, res, next) => {
-  const standardFields = ['email, password'];
-  const isMissingFields = standardFields.some((el) => !Object.keys(req.body).includes(el));
+  const { email, password } = req.body;
+  // const isMissingFields = standardFields.some((el) => !Object.keys(req.body).includes(el));
+  const isMissingFields = !email || !password;
   if (isMissingFields) {
     return res.status(400).json({ message: 'Some required fields are missing' });
   }
   next();
 };
 
-const validateUser = async (req, res, next) => {
+/* const validateUser = async (req, res, next) => {
   const { email, password } = req.body;
-  const checkUser = await User.findOne({ email, password });
-  if (!checkUser) {
+  const user = await User.findOne({ email, password });
+  if (!user) {
     return res.status(400).json({ message: 'Invalid fields' });
   }
+  req.user = user;
   next();
-};
+}; */
 
-module.exports = { validateUserFields, validateUser };
+module.exports = { validateUserFields };
