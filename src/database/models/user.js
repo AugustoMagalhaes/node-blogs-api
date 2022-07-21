39970159b69335544d1a1c3174e15b824b7ec63b
AugustoMagalhaes <augustommg@gmail.com>
2022-07-21 07:02:00 -0300

    feat: implementa model blogPost e cria a associaçao entre este e o model user.

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,20 +1,24 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-	const User = sequelize.define(
-		'User',
-		{
-			id: { type: DataTypes.INTEGER, primaryKey: true },
-			displayName: DataTypes.STRING,
-			email: DataTypes.STRING,
-			password: DataTypes.STRING,
-			image: DataTypes.STRING,
-		},
-		{
-			timestamps: false,
-			tableName: 'Users',
-		},
-	);
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'Users',
+    },
+  );
 
-	return User;
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'blogPosts' });
+  };
+
+  return User;
 };
