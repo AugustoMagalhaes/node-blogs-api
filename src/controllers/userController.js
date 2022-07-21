f0a707edc78b98e15939a28276b51157e3e0bfe7
AugustoMagalhaes <augustommg@gmail.com>
2022-07-21 06:21:27 -0300

    feat: implementa userController.createUser.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,4 +22,25 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { loginUser };
+const createUser = async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+  let error;
+  let httpStatus;
+  let payload;
+  try {
+    const newUser = await userServices.createUser(displayName, email, password, image);
+
+    ({ error, httpStatus, payload } = newUser);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    const token = generateToken(payload);
+    return res.status(httpStatus).json({ token });
+  } catch (err) {
+    return res.status(httpStatus).json({ message: err.message });
+  }
+};
+
+module.exports = { loginUser, createUser };
