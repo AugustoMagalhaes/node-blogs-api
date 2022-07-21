70ae62259e9ef28ac8cdff98a3cf5a01546a0109
AugustoMagalhaes <augustommg@gmail.com>
2022-07-21 06:22:10 -0300

    feat: implementa userRouter no server.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./api');
 
 const loginRouter = require('./routes/loginRouter');
+const userRouter = require('./routes/userRouter');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -12,5 +13,7 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/login', loginRouter);
+
+app.use('/user', userRouter);
 
 app.listen(port, () => console.log('ouvindo porta', port));
