8aba4159616747163f12cafe08703ed3120bb556
AugustoMagalhaes <augustommg@gmail.com>
2022-07-22 03:40:54 -0300

    feat: adiciona rota GET '/user' ao userRouter.

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,9 @@ const express = require('express');
 const userRouter = express.Router();
 const { validateUserFields } = require('../middlewares/validateUser');
 const userController = require('../controllers/userController');
+const { validateTokenField } = require('../middlewares/validateToken');
+
+userRouter.get('/', validateTokenField, userController.getAllUsers);
 
 userRouter.post('/', validateUserFields, userController.createUser);
 
