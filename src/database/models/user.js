1fb01280aa7c9ffe9eab9b676ab354507285d64f
AugustoMagalhaes <augustommg@gmail.com>
2022-07-22 05:03:32 -0300

    fix: adiciona 'autoIncrement: true' em models 'User' e 'Category'.

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -4,7 +4,11 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
-      id: { type: DataTypes.INTEGER, primaryKey: true },
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       displayName: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
