958419b5ddcd86ea2cbf166d649563b4978efbdc
AugustoMagalhaes <augustommg@gmail.com>
2022-07-22 05:05:30 -0300

    feat: adiciona rota categoryRouter ao server.

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = require('./api');
 
 const loginRouter = require('./routes/loginRouter');
 const userRouter = require('./routes/userRouter');
+const categoryRouter = require('./routes/categoryRouter');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -15,5 +16,7 @@ app.get('/', (_request, response) => {
 app.use('/login', loginRouter);
 
 app.use('/user', userRouter);
+
+app.use('/categories', categoryRouter);
 
 app.listen(port, () => console.log('ouvindo porta', port));
