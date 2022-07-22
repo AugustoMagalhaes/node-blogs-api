7d7464b0983a4b8c401886d9db37753583966895
AugustoMagalhaes <augustommg@gmail.com>
2022-07-22 03:39:50 -0300

    chore: altera nome da chave de erro quando nao existe token; muda local de mensagem de erro pra token invalido para o catch.

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,31 +1,29 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { User } = require('../database/models/user');
+const { User } = require('../database/models');
 
 const secret = process.env.JWT_SECRET;
 
 const validateTokenField = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
-    return res.status(401).json({ error: 'Token not found' });
+    return res.status(401).json({ message: 'Token not found' });
   }
 
   try {
     const decoded = jwt.verify(token, secret);
 
-    const user = await User.findOne({
-      where: { email: decoded.data.email, password: decoded.data.password },
-    });
+    const { email, password } = decoded.data;
 
-    if (!user) {
-      return res.status(401).json({ message: 'Expired or invalid token' });
-    }
+    const user = await User.findOne({
+      where: { email, password },
+    });
 
     req.user = user;
 
     next();
   } catch (err) {
-    return res.status(401).json({ message: err.message });
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
 
