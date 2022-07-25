c3b45b6f950e2c663c7c08550295f3c8b9bbb1c0
AugustoMagalhaes <augustommg@gmail.com>
2022-07-25 15:55:06 -0300

    feat: implementa rota postRouter em server.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const app = require('./api');
 const loginRouter = require('./routes/loginRouter');
 const userRouter = require('./routes/userRouter');
 const categoryRouter = require('./routes/categoryRouter');
+const postRouter = require('./routes/postRouter');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -18,5 +19,7 @@ app.use('/login', loginRouter);
 app.use('/user', userRouter);
 
 app.use('/categories', categoryRouter);
+
+app.use('/post', postRouter);
 
 app.listen(port, () => console.log('ouvindo porta', port));
